55e92a7795d697a96920eed4ac47aafbe001b6c1
MrFarhan <muhammadfarhan012345@gmail.com>
2022-12-22 19:00:48 +0500

    Updated folder structure

---
+++ routes/todoRoute.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/todoModel");
+
+router.route("/create").post((req, res) => {
+  const title = req.body.title;
+  const temp = new Todo({ title: title });
+  temp.save();
+});
+
+// router.route("/deleteAll").delete((req, res) => {
+//     Todo.deleteOne({ _id: "ObjectId(60c1161a4b0f082b70e225f3)" })
+
+// })
+
+router.route("/delete/:id").delete((req, res) => {
+  const id = req.params.id;
+
+  Todo.findByIdAndDelete({ _id: id }, (req, res, err) => {
+    if (!err) {
+      console.log("success fully deleted item");
+    } else {
+      console.log("error occured while deleting todo , ", err);
+    }
+  });
+});
+
+router.route("/deleteall").delete(async (req, res) => {
+  await Todo.remove({}, (req, res, err) => {
+    if (!err) {
+      console.log("success fully deleted all");
+    } else {
+      console.log("error occured while deleting all todo , ", err);
+    }
+  });
+});
+
+router.route("/put/:id").put((req, res) => {
+  const title = req.body.title;
+  const id = req.params.id;
+
+  Todo.findByIdAndUpdate(
+    { _id: id },
+    { $set: { title: title } },
+    (req, res, err) => {
+      if (!err) {
+        console.log("success fully updated Todo");
+      } else {
+        console.log("error occured while updating todo , ", err);
+      }
+    }
+  );
+});
+
+router.route("/todos").get((req, res) => {
+  Todo.find().then((todo) => res.json(todo));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const path = require("path");
+
+const PORT = process.env.PORT || 4000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+// to redirect frontend
+// app.use(express.static(path.join(__dirname, "/Web/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/Web/build/index.html"));
+// });
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(require("./routes/todoRoute.js"));
+const mongo = require("./db");
+
+const connectToMongoDB = async () => {
+  await mongo().then(() => {
+    try {
+      console.log("connect to mongodb! :) ");
+    } catch (error) {
+      console.log("error is", error);
+    } finally {
+      //   mongoose.connection.close();
+    }
+  });
+};
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log("server is running on ", PORT);
+});
