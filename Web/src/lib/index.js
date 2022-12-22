af0625b4df323480110fa7f48a6923c06e901147
MrFarhan <muhammadfarhan012345@gmail.com>
2022-12-23 00:29:44 +0500

    Attached frontend with backend on * api call and minor changes

--- Web/src/lib/index.js
+++ Web/src/lib/index.js
@@ -1,1 +1,8 @@
-export const baseURL = 'http://localhost:4000'
+const dev = "http://localhost:4000";
+const prod = "https://mern-todo.surge.sh";
+
+export const baseURL =
+  window.location.hostname.split(":")[0] === "localhost" ||
+  window.location.hostname.includes("192")
+    ? dev
+    : prod;
--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const corsOptions = {
 };
 
 // to redirect frontend
-// app.use(express.static(path.join(__dirname, "/Web/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/Web/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "/Web/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/Web/build/index.html"));
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
